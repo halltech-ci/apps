ac28fb51c0ed447868d2a36c119d4ce890ffb151
Maurice ATCHE <maurice.athce@halltech-africa.com>
2020-04-14 22:31:42 +0000

    Add seniority in hr employee form

--- custom_apps/models/hr_employee.py
+++ custom_apps/models/hr_employee.py
@@ -5,15 +5,17 @@ from odoo import models, fields, api
 class HrEmployee(models.Model):
     _inherit="hr.employee"
     
-    
     hiring_date = fields.Date(string='Hiring Date', required=True, default=fields.Date.today())
-    """
-    seniority = fields.Integer(string="Seniority", default='_compute_seniority')    
+    seniority = fields.Integer(string="Seniority", store=True, compute='_compute_seniority')
+      
     @api.depends('hiring_date')
     def _compute_seniority(self):
         today = fields.Date.today()
         today_date = fields.Date.from_string(today)
-        age = today_date - self.hiring_date
-        seniority = int(age.days/366)
-    """
-    #seniority = fields.Integer(string="Seniority", default='_compute_seniority')
+        for rec in self:
+            age = today_date - fields.Date.from_string(rec.hiring_date)
+            rec.seniority = int(age.days/366)
+
+    
+    
+    
