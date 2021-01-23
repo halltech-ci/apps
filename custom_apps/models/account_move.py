58d52692284756e0129ca69d6e0d8d6f038c950d
halltech-ci <maurice.atche@halltech-africa.com>
2021-01-23 21:58:52 +0000

    Modification du module custom_app

--- custom_apps/models/account_move.py
+++ custom_apps/models/account_move.py
@@ -3,12 +3,12 @@
 from odoo import models, fields, api
 
 
-class AccountMoveLine(models.Model):
+class AccountMove(models.Model):
     _inherit = "account.move.line"
     
     etiquet_analytic_id = fields.Many2one("account.analytic.tag",string="Etiquette")
     
-    account_account_id = fields.Many2one(related='etiquet_analytic_id.account_id',string="Account")
+    account_id_cle = fields.Many2one(related='etiquet_analytic_id.account_id',string="Account")
     
     
     
