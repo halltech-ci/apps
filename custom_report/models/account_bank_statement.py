2ae4b911111a4f35838adbf7c26b30c297b7acfb
halltech-ci <maurice.atche@halltech-africa.com>
2021-02-08 21:35:30 +0000

    Adding methode in account_bank_statement

--- custom_report/models/account_bank_statement.py
+++ custom_report/models/account_bank_statement.py
@@ -6,5 +6,14 @@ from odoo import models, fields, api
 class AccountBankStatement(models.Model):
     _inherit = 'account.bank.statement'
     
-    def button_report_cash(self):
-        pass
+    def button_cash_report(self):
+        return {
+                'type': 'ir.actions.act_window',
+                'name': 'Rapport de caisse',
+                'target': 'new', #use 'current' for not opening in a dialog
+                'res_model': 'account.cash.report.wizard',
+                #'res_id': self.env['stock.request.order'].search([('project_task', '=', self.id)]).id,
+                #'view_id': 'view_xml_id',#optional
+                'view_type': 'form',
+                'views': [[False,'form']],
+                };
