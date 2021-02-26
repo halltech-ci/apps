b430ceb4cde6a22ee33849ec213f2343f3b44601
halltech-ci <maurice.atche@halltech-africa.com>
2021-02-26 06:16:42 +0000

    Modifying hta_custom_project create methode

--- hta_custom_project/models/project.py
+++ hta_custom_project/models/project.py
@@ -13,13 +13,26 @@ class Project(models.Model):
         ("project_unique_code", "UNIQUE (code)", _("The code must be unique!"))
     ]
     
-    @api.model_create_multi
+    """@api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
             if vals.get("code", "/") == "/":
                 vals["code"] = self.env["ir.sequence"].next_by_code("project.code")
         return super().create(vals_list)
+    """
     
+    @api.model
+    def create(self, vals):
+        # Prevent double project creation
+        if vals.get('code', '/') == '/':
+            self = self.with_context(mail_create_nosubscribe=True)
+            vals['code'] = self.env['ir.sequence'].next_by_code('project.code') or '/'
+            project = super(Project, self).create(vals)
+            if not vals.get('subtask_project_id'):
+                project.subtask_project_id = project.id
+            if project.privacy_visibility == 'portal' and project.partner_id:
+                project.message_subscribe(project.partner_id.ids)
+        return project
     
     def copy(self, default=None):
         self.ensure_one()
@@ -27,7 +40,7 @@ class Project(models.Model):
             default = {}
         default["code"] = self.env["ir.sequence"].next_by_code("project.code")
         return super().copy(default)
-
+    
     def name_get(self):
         result = super().name_get()
         new_result = []
