787c602ad48b71ef475b7b55c7bf31f3f70d98cf
halltech-ci <maurice.atche@halltech-africa.com>
2021-02-28 23:11:07 +0000

    Copying purchase from custom_apps to purchase_request_custom

--- purchase_request_custom/__manifest__.py
+++ purchase_request_custom/__manifest__.py
@@ -21,8 +21,6 @@
 
     # any module necessary for this one to work correctly
     'depends': ['purchase_request',
-               'custom_apps',
-               'hta_custom_project',
                ],
 
     # always loaded
