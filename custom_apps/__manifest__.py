ba43bc192c7a15f8303f9486d77fcafe25ae86ed
Jonas Kacou <jonasgnangbi@gmail.com>
2021-03-01 11:05:05 +0000

    Modify module custom_report and custom_apps

--- custom_apps/__manifest__.py
+++ custom_apps/__manifest__.py
@@ -22,6 +22,7 @@
     # any module necessary for this one to work correctly
     'depends': ['sale',
                 'hta_custom_project', 
+                'sale_order_secondary_unit',
                 ],
 
     # always loaded
