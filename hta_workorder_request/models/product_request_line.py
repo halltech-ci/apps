daf9ab7c0db07a0e19c291d505bf1b24718b6d09
halltech-ci <maurice.atche@halltech-africa.com>
2021-03-03 00:14:42 +0000

    Adding stock move in hta_workorder_request

--- hta_workorder_request/models/product_request_line.py
+++ hta_workorder_request/models/product_request_line.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
+from odoo import api, fields, models, _
+from odoo.tools.float_utils import float_compare
+from dateutil import relativedelta
 from odoo.exceptions import UserError, ValidationError
 
 REQUEST_STATE = [('draft', 'Draft'),
@@ -84,6 +86,64 @@ class ProductRequestLine(models.Model):
                 for move in line.move_ids:
                     qty += move.quantity_done
             line.qty_done = qty
+            
+    def _prepare_stock_moves(self, picking):
+        """ Prepare the stock moves data for one product line. This function returns a list of
+        dictionary ready to be used in stock.move's create()
+        """
+        self.ensure_one()
+        res = []
+        if self.product_id.type not in ['product', 'consu']:
+            return res
+        qty = 0.0
+        price_unit = self._get_stock_move_price_unit()
+        moves = self.move_ids.filtered(lambda r: r.state != 'cancel' and not r.scrapped and self.product_id == r.product_id)
+        for move in moves:
+            qty -= move.product_uom._compute_quantity(move.product_uom_qty, self.product_uom, rounding_method='HALF-UP')
+        description_picking = self.product_id.with_context(lang=self.request_id.project_id.partner_id.lang or self.env.user.lang)._get_description(self.request_id.picking_type_id)
+        template = {
+            # truncate to 2000 to avoid triggering index limit error
+            # TODO: remove index in master?
+            'name': (self.name or '')[:2000],
+            'product_id': self.product_id.id,
+            'product_uom': self.product_uom_id.id,
+            'date': self.request_id.date_approve,
+            #'date_expected': self.date_planned,
+            'location_id': self.request_id.location_src_id.id,
+            'location_dest_id': self.request_id.location_dest_id.id,
+            'picking_id': picking.id,
+            #'partner_id': self.request_id.dest_address_id.id,
+            #'move_dest_ids': [(4, x) for x in self.move_dest_ids.ids],
+            'state': 'draft',
+            'product_line_id': self.id,
+            'company_id': self.request_id.company_id.id,
+            'price_unit': price_unit,
+            'picking_type_id': self.request_id.picking_type_id.id,
+            #'group_id': self.request_id.group_id.id,
+            'origin': self.request_id.name,
+            #'propagate_date': self.propagate_date,
+            #'propagate_date_minimum_delta': self.propagate_date_minimum_delta,
+            'description_picking': description_picking,
+            #'propagate_cancel': self.propagate_cancel,
+            'route_ids': self.request_id.picking_type_id.warehouse_id and [(6, 0, [x.id for x in self.request_id.picking_type_id.warehouse_id.route_ids])] or [],
+            'warehouse_id': self.request_id.picking_type_id.warehouse_id.id,
+        }
+        diff_quantity = self.product_uom_qty - qty
+        if float_compare(diff_quantity, 0.0,  precision_rounding=self.product_uom_id.rounding) > 0:
+            pr_line_uom = self.product_uom_id
+            quant_uom = self.product_id.uom_id
+            product_uom_qty, product_uom = pr_line_uom._adjust_uom_quantities(diff_quantity, quant_uom)
+            template['product_uom_qty'] = product_uom_qty
+            template['product_uom'] = product_uom.id
+            res.append(template)
+        return res
+        
+    def _create_stock_moves(self, picking):
+        values = []
+        for line in self:
+            for val in line._prepare_stock_moves(picking):
+                values.append(val)
+        return self.env['stock.move'].create(values)
            
     def action_to_approve(self):
         self.request_state = "to_approve"
