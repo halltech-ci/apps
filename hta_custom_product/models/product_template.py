3f0e6e0136190e6145cc19dff604127ad9e5866d
halltech-ci <maurice.atche@halltech-africa.com>
2021-03-07 19:29:55 +0000

    Adding unique code for product

--- hta_custom_product/models/product_template.py
+++ hta_custom_product/models/product_template.py
@@ -7,9 +7,8 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
     #Make default_code field mandatory
     def _get_sale_uom_id(self):
-        return self.env["product.uom"].search([], limit=1, order='id').id
+        return self.env["uom.uom"].search([], limit=1, order='id').id
     
     type = fields.Selection(default='service')
-    uom_so_id = fields.Many2one('product.uom', 'Sale Unit of Measure', default=_get_sale_uom_id, 
+    uom_so_id = fields.Many2one('uom.uom', 'Sale Unit of Measure', default=_get_sale_uom_id, 
         help="Default Unit of Measure used for sale orders. It must be in the same category than the default unit of measure.")
-© 2021 GitHub, Inc.
