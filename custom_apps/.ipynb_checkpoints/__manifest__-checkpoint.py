387a0dbd5224ffb1915646dd2f6cbe79d53180fa
halltech-ci <maurice.atche@halltech-africa.com>
2021-03-08 10:06:04 +0000

    Adding product_code on sale_order in hta_custom_sale

--- custom_apps/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ custom_apps/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -17,17 +17,26 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '13.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale_management','project',],
+    'depends': ['hta_custom_sale', 
+                ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/account_cash_views.xml',
+        #'views/account_analytic_tag_views.xml',
+        'views/account_move_views.xml',
+        'views/account_account_views.xml',
         'views/project_views.xml',
+        #'views/sale_order_views.xml',
+        #data sequence
+        'data/sale_ir_sequence.xml',
+        'data/purchase_ir_sequence.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- custom_apps/__manifest__.py
+++ custom_apps/__manifest__.py
@@ -17,11 +17,10 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '13.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale',
-                'hta_custom_project', 
+    'depends': ['hta_custom_sale', 
                 ],
 
     # always loaded
