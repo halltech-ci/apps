896c00e6883d775f72f52b7910c67198cef27587
halltech-ci <maurice.atche@halltech-africa.com>
2021-03-15 09:36:47 +0000

    Adding remise_total in hta_custom_sale

--- hta_custom_sale/__manifest__.py
+++ hta_custom_sale/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '13.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale_management',
+    'depends': ['sale_order_general_discount',
                'hta_custom_project',
                ],
 
