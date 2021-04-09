057176f2372691895742dfcf9a196504c373cf1f
halltech-ci <maurice.atche@halltech-africa.com>
2021-04-09 09:01:34 +0000

    Setting product type default to service

--- hta_custom_product/__manifest__.py
+++ hta_custom_product/__manifest__.py
@@ -16,12 +16,12 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Product',
+    'category': 'Purchase',
     'version': '13.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
-                'stock',
+                'purchase',
                ],
 
     # always loaded
