72038de0dd598fb833ef00f5d1252dae67f5da20
halltech-ci <maurice.atche@halltech-africa.com>
2021-04-18 22:56:27 +0000

    Adding custom reconcile button in account statement line

--- expense_management/models/account_bank_statement.py
+++ expense_management/models/account_bank_statement.py
@@ -11,18 +11,20 @@ class AccountBankStatement(models.Model):
         return {
                 'type': 'ir.actions.act_window',
                 'name': 'Lettrage Caisse',
-                'target': 'main', #use 'current' for not opening in a dialog
                 'res_model': 'account.bank.statement.line',
-                'domain': [('statement_id', '=', self.id)],
-                'view_type': 'tree',
+                "domain": [["statement_id", "=", self.id], ['journal_entry_ids', '=', False], 
+                       ['amount', '!=', 0]
+                ],
                 'views': [[view_id,'tree']],
                 };
+    
     
 class AccountBankStatementLine(models.Model):
     _inherit = "account.bank.statement.line"
     
     analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account', ondelete='set null')
     analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic Tags', 
+        domain="['|', ('company_id', '=', company_id), ('company_id', '=', False)]", 
         relation='account_statement_model_analytic_tag_rel'
     )
     
