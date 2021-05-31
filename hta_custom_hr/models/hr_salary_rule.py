ae49dde16143cc8361066591a999379f23334a67
halltech-ci <maurice.atche@halltech-africa.com>
2021-05-31 00:55:43 +0000

    Adding constrains for duplicate payslip

--- hta_custom_hr/models/hr_salary_rule.py
+++ hta_custom_hr/models/hr_salary_rule.py
@@ -15,12 +15,12 @@ class HrPayslip(models.Model):
     slip_month = fields.Char(string='Slip Month', store=True, compute='compute_slip_month')
     
     #check unique payslip for employee for a given date
-    @api.constrains('slip_month', 'employee_id')
+    """@api.constrains('slip_month', 'employee_id')
     def _unique_slip_id_per_date(self):
         for record in self:
             res = self.env['hr.payslip'].search([('slip_month', '=', record.slip_month), ('employee_id', '=', record.employee_id.id)])
             if len(res) > 1:
-                raise ValidationError("You cannot create duplicate payslip for employee {0}. Payslip already exist.".format(record.employee_id.name))
+                raise ValidationError("You cannot create duplicate payslip for employee {0}. Payslip already exist.".format(record.employee_id.name))"""
     
     @api.depends('date_from')
     def compute_slip_month(self):
