cdfa72a4cdc14c1fd6f81d1317a9b62144789cac
ATCHE Maurice <maurice.atche@halltech-africa.com>
2021-07-24 21:49:46 +0000

    Adding product_tmpl_id in purchase_request_line

--- hta_purchase_request_variant_configurator/models/purchase_request_line.py
+++ hta_purchase_request_variant_configurator/models/purchase_request_line.py
@@ -6,3 +6,11 @@ from odoo import models, fields, api
 class PurchaseRequestLine(models.Model):
     _inherit = ["purchase.request.line", "product.configurator"]
     _name = "purchase.request.line"
+    
+    
+    product_tmpl_id = fields.Many2one(
+        store=True,
+        readonly=False,
+        related=False,
+        string="Product Template (no related)",
+    )
