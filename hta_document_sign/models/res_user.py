d2399a47dd9ff8553f279d2170a62062aa090b20
Jonas Kacou <jonasgnangbi@gmail.com>
2021-08-12 14:47:41 +0000

    Modify fields for res_user.py

--- hta_document_sign/models/res_user.py
+++ hta_document_sign/models/res_user.py
@@ -6,5 +6,5 @@ from odoo import models, fields, api
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    hta_doc_signature = fields.Binary(string="Digital Signature")
-    hta_doc_initials = fields.Binary(string="Digitial Initials")
+    hta_doc_signature = fields.Binary(string="Signature Document")
+    hta_doc_initials = fields.Binary(string="Signature Initials")
