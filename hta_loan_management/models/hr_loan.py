ecd2bdbc2197a370571d634060dc306f9e3b1ab4
ATCHE Maurice <maurice.atche@halltech-africa.com>
2021-09-12 22:56:00 +0000

    Adding loan management

--- hta_loan_management/models/hr_loan.py
+++ hta_loan_management/models/hr_loan.py
@@ -20,6 +20,8 @@ class LoanRequest(models.Model):
     disburse_amount = fields.Monetary('Loan Amount', currency_field='currency_id', compute="_compute_disburse_amount")
     duration = fields.Integer(string="Duration")
     end_amount = fields.Monetary('Loan Amount', currency_field='currency_id', tracking=True)
+    company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True, states={'draft': [('readonly', False)], 'refused': [('readonly', False)]}, default=lambda self: self.env.company)
+    currency_id = fields.Many2one('res.currency', string='Currency', readonly=True, states={'draft': [('readonly', False)]}, default=lambda self: self.env.company.currency_id)
     
     @api.depends("loan_amount", "duration")
     def _compute_disburse_amount(self):
