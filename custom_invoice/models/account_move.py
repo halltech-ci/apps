11a98b238bc61657c5e91175b53be5bfcacbc9d5
sagno <sagnodaouda@gmail.com>
2021-09-25 08:59:27 +0000

    creation du rapoprt xlsx

--- custom_invoice/models/account_move.py
+++ custom_invoice/models/account_move.py
@@ -36,52 +36,7 @@ class AccountMove(models.Model):
             
             
             
-class PartnerXlsx(models.AbstractModel):
-    _name = 'report.custom_invoice.report_invoice_xlsx'
-    _description = 'Facture Report Excel'
-    _inherit = 'report.report_xlsx.abstract'
 
-    def generate_xlsx_report(self, workbook, data, partners):
-        sheet = workbook.add_worksheet('Patner ID')
-        bold = workbook.add_format({'bold': True})
-        row = 10
-        col = 0
-        sheet.set_column('A:A',12)
-        sheet.set_column('B:B',12)
-        sheet.set_column('C:C',15)
-        sheet.set_column('D:D',20)
-        sheet.set_column('E:E',15)
-        sheet.set_column('F:F',12)
-        sheet.set_column('G:G',12)
-        sheet.set_column('H:H',15)
-        sheet.set_column('I:I',12)
-        sheet.set_column('J:J',15)
-        sheet.set_column('K:K',12)
-        for obj in partners:
-            row += 1
-            sheet.write(10,0, 'Numero',bold)
-            sheet.write(row, col, obj.name)
-            sheet.write(10,1, 'Customer',bold)
-            sheet.write(row, col +1, obj.invoice_partner_display_name)           
-            sheet.write(10,2, 'Invoice Date',bold)
-            sheet.write(row, col +2, obj.invoice_date)           
-            sheet.write(10,3, 'Source Document',bold)
-            sheet.write(row, col +3, obj.invoice_origin)    
-            #sheet.write(10,4, 'Sales Persons',bold)
-            #sheet.write(row, col +1, obj.invoice_user_id)            
-            #sheet.write(10,5, 'Company',bold)
-            #sheet.write(row, col +1, obj.company_id)           
-            #sheet.write(10,6, 'Due Date',bold)
-            #sheet.write(row, col +1, obj.invoice_date_due)            
-            #sheet.write(10,7, 'Tax Excluded',bold)
-            #sheet.write(row, col +1, obj.amount_untaxed_signed)            
-            #sheet.write(10,8, 'Total',bold)
-            #sheet.write(row, col +1, obj.amount_total_signed)           
-            #sheet.write(10,9, 'Amount Due',bold)
-            #sheet.write(row, col +1, obj.amount_residual_signed)          
-            #sheet.write(10,10, 'Status',bold)
-            #sheet.write(row, col +1, obj.state)
-            
            
             
             
