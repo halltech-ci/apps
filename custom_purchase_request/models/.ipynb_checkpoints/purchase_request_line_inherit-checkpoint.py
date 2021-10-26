7aa3e5208d36b63373b9e7d1198952182a0d4e86
Jonas Kacou <jonasgnangbi@gmail.com>
2021-10-26 11:48:16 +0000

    Description for purchase request modify

--- custom_purchase_request/models/.ipynb_checkpoints/purchase_request_line_inherit-checkpoint.py
+++ custom_purchase_request/models/.ipynb_checkpoints/purchase_request_line_inherit-checkpoint.py
@@ -7,12 +7,16 @@ class custom_purchase_request(models.Model):
     _inherit = 'purchase.request.line'
    
     
+    
     @api.onchange("product_id")
     def onchange_product_id(self):
         if self.product_id:
             name = self.product_id.name
             if self.product_id.code:
-                name = "{} ({})".format(self.product_id.product_tmpl_id.name, name)
+                name = "{} (".format(self.product_id.product_tmpl_id.name, name)
+                for no_variant_attribute_value in self.product_id.product_template_attribute_value_ids:
+                    name += "{}".format(no_variant_attribute_value.name + ', ')
+                name += ")"
             if self.product_id.description_purchase:
                 name += "\n" + self.product_id.description_purchase
             self.product_uom_id = self.product_id.uom_id.id
