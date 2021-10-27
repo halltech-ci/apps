f0221f799f74d3540f07ba1c8903eb632b464922
sagno <sagnodaouda@gmail.com>
2021-10-27 18:02:45 +0000

    hta_product models modification

--- hta_produc/models/product_template.py
+++ hta_produc/models/product_template.py
@@ -10,26 +10,26 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
     
    
-    code_prefix = fields.Char(compute='_compute_code_prefix', help="Add prefix to product variant reference (default code)")
+    code_prefix = fields.Char()
     code_mesure = fields.Char()
     
     
     # affichage du name
     @api.onchange("categ_id","code_mesure")
     def _onchange_name_(self):
+        
         self.name = str(self.categ_id.recovery_name) +' '+ str(self.code_mesure) 
-                
     
-    @api.depends("categ_id")
-    def _compute_code_prefix(self):
-        req = self.env['product.template'].search([('categ_id','=',self.categ_id.id)])
-        increment = len(req) + 1
-        converts = str(increment)
-        if len(converts) == 1:
-            converts = '00' + str(converts)
-        if len(converts) == 2:
-            converts = '0' + str(converts)
-        if self.categ_id:
-            self.code_prefix = self.categ_id.code_reference2 + converts
-        else:
-            self.code_prefix = converts
+#     @api.onchange("categ_id")
+#     def _onchange_code_prefix(self):
+#         req = self.env['product.template'].search([('categ_id','=',self.categ_id.id)])
+#         increment = len(req) + 1
+#         converts = str(increment)
+#         if len(converts) == 1:
+#             converts = '00' + str(converts)
+#         if len(converts) == 2:
+#             converts = '0' + str(converts)
+        
+#         self.code_prefix = converts
+
+    
