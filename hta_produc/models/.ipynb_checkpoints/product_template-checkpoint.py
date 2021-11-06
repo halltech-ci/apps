c74f535cada1914dda2d6351e8785acec80741aa
sagno <sagnodaouda@gmail.com>
2021-11-06 13:47:08 +0000

    hta_products

--- hta_produc/models/.ipynb_checkpoints/product_template-checkpoint.py
+++ hta_produc/models/.ipynb_checkpoints/product_template-checkpoint.py
@@ -9,8 +9,6 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
     
     
-    
-    
     code_prefix = fields.Integer()
     caracteristique = fields.Char()
     code_ref = fields.Char()
@@ -46,13 +44,14 @@ class ProductTemplate(models.Model):
             rec.code_referen = converts
         else:
             self.code_referen = converts
+            
        
     @api.depends("categ_id")
     def _compute_barcode(self):
         for rec in self:
             if rec.categ_id:
                 rec.barcode = rec.code_prefix
-            
+                
             
     #affichage du name
     @api.onchange("categ_id","caracteristique")
@@ -71,114 +70,3 @@ class ProductTemplate(models.Model):
                 rec.code_ref = str(rec.categ_id.code_references) + str(rec.code_referen)
             else:
                 rec.code_ref = '%s' %(rec.code_referen)
-    
-#     @api.onchange('code_prefix')
-#     def onchange_code_ref(self):
-#         for rec in self:
-#             if rec.code_prefix:
-#                 rec.code_ref = str(rec.categ_id.code_references) + str(rec.code_prefix)
-#             else:
-#                 rec.code_ref = '%s' %(rec.code_prefix)
-        
-#     @api.onchange('categ_id.code_references')
-#     def onchange_code_ref(self):
-#         for rec in self:
-#             if rec.categ_id:
-#                 rec.code_ref = str(rec.categ_id.code_references) + str(rec.code_prefix)
-#             else:
-#                 rec.code_ref = '%s' %(rec.code_prefix)
-                
-                
-                
-#     def _search_prefix_field(self, operator, value):
-#         return [('code_prefix', operator, value)]
-    
-    
-    
-    
-#     @api.constrains('name')
-#     def _check_name(self):
-#         for rec in self:
-#             article = self.env['product.template'].search([('name','=',rec.name), ('id','!=',rec.id)])
-#             if article:
-#                 raise ValidationError(_("Name %s existe déjà" % rec.name))
-    
-#     @api.constrains('name')
-#     def _check_unique_code_mesure(self):
-#         names = self.search([]) - self
-#         values = [ x.name.lower() for x in names ]
-        
-#         if self.name and self.name.lower() in values:
-#             raise ValidationError(_('article already exists!'))
-	
-#         return True
-
-
-
-    
-
-    
-    #code article reference
-#     @api.model
-#     def create(self, vals):
-#         if vals.get('product_reference', _('New')) == _('New'):
-#             vals['product_reference'] = self.env['ir.sequence'].next_by_code('product_reference.code') or _('New')
-#         result = super(ProductTemplate, self).create(vals)
-#         return result
-    
-    
-#     @api.depends("product_reference")
-#     def _compute_code_prefix(self):
-#         for rec in self:
-#             rec.code_prefix = rec.product_reference
-    
-
-    
-#     @api.depends("categ_id","code_prefix")
-#     def _compute_code_prefix(self):
-#         for rec in self:
-#             rec.code_prefix = rec.product_reference
-
-#     @api.depends("code_prefix","categ_id")
-#     def _compute_code_reference(self):
-#         for rec in self:
-#             if rec.code_prefix == "New":
-#                 rec.code_ref =  str(rec.categ_id.code_reference2)
-#             else:
-#                 rec.code_prefix = rec.code_ref
-                
-                
-#     @api.onchange("code_prefix","categ_id")
-#     def _compute_code_referen(self):
-#         for rec in self:
-#             if rec.categ_id:
-#                 rec.code_referen =  str(rec.categ_id.code_reference)+ str(rec.code_prefix)
-              
-            
-            
-            
-#     def fonctionTranche(self,liste, groupement):
-#         res = ""
-#         cpt = 0
-#         for l in range(0,len(liste)):
-#             res = res + liste[l]
-#             cpt = cpt + 1
-#             if cpt == groupement:
-#                 res = res + "-"
-#                 cpt = 0
-#         return res
-    
-#     @api.onchange("code_referen")
-#     def _compute_code_reference3(self):
-#         for rec in self:
-#             rec.resultat = rec.fonctionTranche(str(rec.code_referen),rec.groupement)
-#             rec.code_reference3 = rec.resultat
-            
-        
-   
-    
-#     @api.onchange("code_ref")
-#     def _compute_code_reference(self):
-#         for rec in self:
-#             rec.resultat = rec.fonctionTranche(str(rec.code_ref),rec.groupement)
-#             rec.code_reference = rec.resultat
