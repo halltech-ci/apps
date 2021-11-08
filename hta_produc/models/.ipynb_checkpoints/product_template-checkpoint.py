f9ea30b57ed698ad669e3b16b555b20383c0dbdb
sagno <sagnodaouda@gmail.com>
2021-11-08 09:44:22 +0000

    hta_products

--- hta_produc/models/.ipynb_checkpoints/product_template-checkpoint.py
+++ hta_produc/models/.ipynb_checkpoints/product_template-checkpoint.py
@@ -91,8 +91,10 @@ class ProductTemplate(models.Model):
         for rec in self:
             if rec.code_concartel:
                 rec.resultat = rec.fonctionTranche(str(rec.code_concartel),int(rec.categ_id.code_range))
-                #rec.code = rec.resultat.rstrip(rec.resultat[-1])
-                #rec.code_ref = rec.code
-                rec.code_ref = rec.resultat
+            if rec.resultat[-1] == '-':
+                rec.code = rec.resultat.rstrip(rec.resultat[-1])
+                rec.code_ref = rec.code
+                #rec.code_ref = rec.resultat    
+                #rec.code_ref = rec.resultat
             else:
                 rec.code_ref = rec.code_referen
