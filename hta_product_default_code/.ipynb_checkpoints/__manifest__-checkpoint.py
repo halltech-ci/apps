1fe54e2cd63537526df5aceb533493986ee96fee
Jonas Kacou <jonasgnangbi@gmail.com>
2021-12-31 09:41:44 +0000

    Default Code Hta

--- hta_product_default_code/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ hta_product_default_code/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['stock'],
+    'depends': ['stock','product'],
 
     # always loaded
     'data': [
--- hta_product_default_code/models/product_template.py
+++ hta_product_default_code/models/product_template.py
@@ -6,15 +6,6 @@ from odoo.exceptions import UserError, ValidationError
 class ProductTemplateCustom(models.Model):
     _inherit = "product.template"
     
-                    
-    @api.onchange("categ_id")
-    def _onchange_attributes_product_id(self):
-        if self.categ_id:
-            attributs_ids = self.categ_id.attribute_ids.ids
-            if attributs_ids: 
-                domain = [('attribute_id','in',attributs_ids)]
-                return  {'domain':{'attribute_line_ids':domain}}
-        return  {'domain':{'attribute_line_ids':[('attribute_id', 'in', [])]}}
     
     @api.depends("categ_id")
     def _depends_attribute(self):
