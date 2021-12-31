c8a0f542e4371fa035a69d082b77171feaed5473
Jonas Kacou <jonasgnangbi@gmail.com>
2021-12-31 09:19:18 +0000

    Modify hta_product_default

--- hta_product_default_code/__manifest__.py
+++ hta_product_default_code/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['stock','product_variant_default_code',"product"],
+    'depends': ['stock','product'],
 
     # always loaded
     'data': [
