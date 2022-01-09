6e7ddc112a019d9b7ca977b8d7481d6a38194d09
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-01-09 16:44:58 +0000

    Adding compute_code in hta_product_code

--- hta_product_code/models/product_attribute.py
+++ hta_product_code/models/product_attribute.py
@@ -13,6 +13,12 @@ class ProductAttribute(models.Model):
     code_length = fields.Integer(default=2, string="Nombre de caractere")
     is_automatic_code = fields.Boolean(default=True, string="Automatique/Manuel")
     code_compute_parameter = fields.Char(string="Parametre")
+    last_code = fields.Integer(string="Last Value Code", compute="_compute_last_value_code")
+    
+    def _compute_last_value_code(self):
+        for rec in self:
+            code = rec.mapped('value_ids').sort(reverse=True)
+            self.last_code = int([0]) or 1
     
     def _recuperate_compute_parameter(self):
         if self.code_compute_parameter:
@@ -20,42 +26,25 @@ class ProductAttribute(models.Model):
                      for x, y in (element.split(':')
                                   for element in self.code_compute_parameter.split(',')))
             return convertedDict
+        
+    def _compute_code(self):
+        for rec in self:
+            parametre = rec._recuperate_compute_parameter()
+            val = 1
+            if "incr" in parametre:
+                val = int(parametre.get('incr'))
+                code = val
+                for line in rec.value_ids:
+                    line.code = code 
+                    code += val
+    
+    def write(self, line_values):
+        res = super(ProductAttribute, self).write(line_values)
+        self._compute_code()
+        return res
     
 class ProductAttributeValue(models.Model):
     _inherit = "product.attribute.value"
     
-    def increment(self, val, attribute_id):
-        #domaine = [()]
-        last_code = self.env['product.attribute'].search([('id', '=', attribute_id)]).mapped('value_ids.code').sort(reverse=True)
-        code ="0"
-        if last_code :
-            code = last_code[0] + val
-        else:
-            code = last_code
-        return code
-    
-    @api.onchange('name')
-    def _onchange_name(self):
-        if self.name:
-            param = self.attribute_id._recuperate_compute_parameter()
-            attribute_id = self.attribute_id._origin.id
-            if param:
-                if 'incr' in param:
-                    val = param.get('incr')
-                    #self.increment(param.get('incr'))
-                    self.code = self._origin.id#self.increment(val, attribute_id)
-                if 'tronc' in param:
-                    val = param.get('tronc')
-                    self.code = self.name[0:val]
-                if 'pre' in param:
-                    val = param.get('pre')
-                    self.code = val + self.name
-    
-    
-#     @api.onchange('name')
-#     def _onchange_name(self):
-#         if self.name:
-#             self.code = self.name[0:2]
-    
-    code = fields.Char(default=_onchange_name, string="Code", store=True)
+    code = fields.Char(string="Code", store=True)
     
