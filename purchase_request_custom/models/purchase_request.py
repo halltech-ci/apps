d970fb4715ca91ba5ab9c5b816e5617b771dfebe
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-02-07 21:25:45 +0000

    Making name invible in purchase_request_line

--- purchase_request_custom/models/purchase_request.py
+++ purchase_request_custom/models/purchase_request.py
@@ -90,6 +90,7 @@ class PurchaseRequestLine(models.Model):
     product_tmpl_id = fields.Many2one("product.template", related="product_id.product_tmpl_id")
     #product_attribute_ids = fields.Many2many('product.attribute.', related="product_tmpl_id.product_attribute_ids")
     attribute_line_ids = fields.One2many("product.template.attribute.line", related="product_tmpl_id.attribute_line_ids")
+
     
     @api.onchange("product_id")
     def onchange_product_id(self):
@@ -97,10 +98,9 @@ class PurchaseRequestLine(models.Model):
             if rec.product_id:
                 name = rec.product_id.name
                 if rec.product_id.code:
-                    name = "{} (".format(rec.product_id.product_tmpl_id.name, name)
+                    name = "{} ".format(rec.product_id.product_tmpl_id.name)
                     for no_variant_attribute_value in rec.product_id.product_template_attribute_value_ids:
                         name += "{}".format(no_variant_attribute_value.name + ', ')
-                    name += ")"
                 if rec.product_id.description_purchase:
                     name += "\n" + rec.product_id.description_purchase
                 rec.product_uom_id = rec.product_id.uom_id.id
