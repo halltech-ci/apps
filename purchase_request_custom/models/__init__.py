acb86846e12f28bd7f02b639de44f41d42e950b3
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-02-14 00:58:23 +0000

    Adding default value for company in product_template

--- purchase_request_custom/models/__init__.py
+++ purchase_request_custom/models/__init__.py
@@ -3,6 +3,6 @@
 from . import models
 from . import purchase_request
 from . import purchase_order
-from . import product
+#from . import product
 from . import purchase_request_allocation
 #from . import res_partner
