5e298d4481af83df01b87d52d7a2ccad6abc7598
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-02-23 07:31:59 +0000

    Overriding create in hta_custom_product

--- hta_custom_product/models/product.py
+++ hta_custom_product/models/product.py
@@ -2,39 +2,6 @@
 
 from odoo import models, fields, api, _
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    
-    @api.model_create_multi
-    def create(self, vals_list):
-        ''' Store the initial standard price in order to be able to retrieve the cost of a product template for a given date'''
-        templates = super(ProductTemplate, self).create(vals_list)
-        if "create_product_product" not in self._context:
-            templates._create_variant_ids()
-
-        # This is needed to set given values to first variant after creation
-        for template, vals in zip(templates, vals_list):
-            related_vals = {}
-            if vals.get('product_variant_id'):
-                related_vals['name'] = vals['product_variant_id']
-            if vals.get('barcode'):
-                related_vals['barcode'] = vals['barcode']
-            if vals.get('default_code'):
-                related_vals['default_code'] = vals['default_code']
-            if vals.get('standard_price'):
-                related_vals['standard_price'] = vals['standard_price']
-            if vals.get('volume'):
-                related_vals['volume'] = vals['volume']
-            if vals.get('weight'):
-                related_vals['weight'] = vals['weight']
-            # Please do forward port
-            if vals.get('packaging_ids'):
-                related_vals['packaging_ids'] = vals['packaging_ids']
-            if related_vals:
-                template.write(related_vals)
-
-        return templates
-
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -50,7 +17,7 @@ class ProductTemplate(models.Model):
         for template, vals in zip(templates, vals_list):
             related_vals = {}
             if vals.get('display_name'):
-                related_vals['name'] = vals['display_name']
+                related_vals['description_sale'] = vals['display_name']
             if vals.get('barcode'):
                 related_vals['barcode'] = vals['barcode']
             if vals.get('default_code'):
@@ -72,6 +39,16 @@ class ProductTemplate(models.Model):
 class ProductProduct(models.Model):
     _inherit = "product.product"
     
+    
+    """@api.model_create_multi
+    def create(self, vals_list):
+        products = super(ProductProduct, self.with_context(create_product_product=True)).create(vals_list)
+        # `_get_variant_id_for_combination` depends on existing variants
+        if vals.get('product_variant_id'):
+            related_vals['name'] = vals['product_variant_id']
+        self.clear_caches()
+        return products
+    """
     
     def name_get(self):
         # TDE: this could be cleaned a bit I think
