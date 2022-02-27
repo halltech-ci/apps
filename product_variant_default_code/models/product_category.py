6d43df87b2f58e657d711d593a7006bf5d551bdb
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-02-27 18:57:30 +0000

    Updating product_variant_default_code

--- product_variant_default_code/models/product_category.py
+++ product_variant_default_code/models/product_category.py
@@ -10,33 +10,7 @@ class ProductCategory(models.Model):
 
     description = fields.Text()
     category_code = fields.Char(index=True,)
-    related_code = fields.Char(string='Related Code', 
-                               #compute = '_compute_related_code', 
-                               recursive=True, store=True, search='_search_related_field',
-                              )
-    attribute_lines = fields.One2many('product.attribute.line', 'category_id')
-    reference_mask = fields.Char(string="Variant reference mask", copy=False,
-        help="Reference mask for building internal references of a "
-        "variant generated from this template.\n"
-        "Example:\n"
-        "A product named ABC with 2 attributes: Size and Color:\n"
-        "Product: ABC\n"
-        "Color: Red(r), Yellow(y), Black(b)  #Red, Yellow, Black are "
-        "the attribute value, `r`, `y`, `b` are the corresponding code\n"
-        "Size: L (l), XL(x)\n"
-        "When setting Variant reference mask to `[Color]-[Size]`, the "
-        "default code on the variants will be something like `r-l` "
-        "`b-l` `r-x` ...\n"
-        "If you like, You can even have the attribute name appear more"
-        " than once in the mask. Such as,"
-        "`fancyA/[Size]~[Color]~[Size]`\n"
-        " When saved, the default code on variants will be "
-        "something like \n"
-        ' `fancyA/l~r~l` (for variant with Color "Red" and Size "L") '
-        ' `fancyA/x~y~x` (for variant with Color "Yellow" and Size "XL")'
-        '\nNote: make sure characters "[,]" do not appear in your '
-        "attribute name",
-    )
+    #attribute_lines = fields.One2many('product.attribute.line', 'category_id')
     
     """
     @api.depends("attribute")
@@ -45,7 +19,9 @@ class ProductCategory(models.Model):
             if rec.attribute:
                 rec.value_ids = self.env["product.attribute"].search([('id', "=", rec.attribute.id)]).mapped('value_ids')
 
-    """    
+    """
+    
+"""    
 class ProductAttributeLine(models.Model):
     _name ="product.attribute.line"
     _description = "Product attribute define in product category"
@@ -61,3 +37,4 @@ class ProductAttributeLine(models.Model):
     def _onchange_attribute(self):
         if self.attribute:
             self.value_ids = self.env["product.attribute"].search([('id', "=", self.attribute.id)]).mapped('value_ids')
+"""
