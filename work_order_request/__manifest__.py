c417f48ffa8a0fd074b53333b50b9bba319d0cbb
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-04-22 05:20:31 +0000

    Adding work_order_request from projet_stock

--- work_order_request/__manifest__.py
+++ work_order_request/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "workorder_request",
+    'name': "work_order_request",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,29 +16,20 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Warehouse Management',
+    'category': 'Project Management',
     'version': '13.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['stock',
-                'product',
-                'project'
-               ],
+    'depends': ['project', 'stock'],
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/workorder_request_views.xml',
-        'views/workorder_request_menu.xml',
+        #views
         'views/project_views.xml',
-        #data
-        'data/workorder_ir_sequence.xml',
-        #report
-        'report/workorder_report.xml',
-        'report/workorder_request_report.xml',
-        
+        'views/stock_move_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ work_order_request/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import project
+from . import project_task
+from . import stock_move
+from . import stock_scrap
+from . import stock_analytic_line
