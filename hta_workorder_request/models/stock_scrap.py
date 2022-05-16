2cc73646fa2d076d708f627a5eb67b8b01734040
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-05-16 08:36:02 +0000

    Adding stock_scrap in workorder request

--- hta_workorder_request/models/stock_scrap.py
+++ hta_workorder_request/models/stock_scrap.py
@@ -7,7 +7,7 @@ class StockMove(models.Model):
     _inherit = "stock.scrap"
 
     product_request = fields.Many2one("product.request", string="Task", check_company=True)
-    task_id = fields.Many2one('project.task', related="product_request.project.task_id")
+    task_id = fields.Many2one('project.task', related="product_request.project_task_id")
 
     @api.onchange("product_request")
     def _onchange_task_id(self):
