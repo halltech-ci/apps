2f346b652978bfe6c1c5b600e1132b2378a7fbf1
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-05-31 18:28:09 +0000

    Overriding timesheet_postprocess in analytic_line in workorder

--- hta_workorder_request/models/account_analytic.py
+++ hta_workorder_request/models/account_analytic.py
@@ -14,3 +14,11 @@ class AccountAnalyticLine(models.Model):
     product_line = fields.One2many('product.request.line', 'analytic_line', string="Product line")
     planned_date = fields.Date('Planned Date')
     
+    def _timesheet_postprocess_values(self, values):
+        res = super(AccountAnalyticLine, self)._timesheet_postprocess_values(values)
+        # Delete the changes in amount if the analytic lines
+        # come from product_line.
+        for key in (self.filtered(lambda x: x.product_line).ids):
+            res[key].pop('amount', None)
+        return res
+    
