a0cb533d74ea7c0c0ef3f11167d6da5d678dbaa5
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-06-06 00:59:01 +0000

    Deleting stock dependancy in hta_custom_product

--- hta_custom_product/__manifest__.py
+++ hta_custom_product/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['product', 'stock'],
+    'depends': ['product',],
 
     # always loaded
     'data': [
