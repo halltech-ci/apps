c900607a7a60bf9a7ec78fe79aa92953686876b4
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-06-07 08:16:08 +0000

    Adding description in workorder_sale in workorder_management

--- workorder_management/workorder_sale/models/workorder_sale.py
+++ workorder_management/workorder_sale/models/workorder_sale.py
@@ -10,6 +10,7 @@ class WorkorderSale(models.Model):
     
     name = fields.Char()
     code = fields.Char()
+    description = fields.Html(help="Indiquer ici les caracteristiques de l'ouvrage.")
     line_ids = fields.One2many('workorder.line', 'workorder_id')
     company_id = fields.Many2one('res.company', string='Company', required=True, index=True, readonly=True, default=lambda self: self.env.company)
     currency_id = fields.Many2one('res.currency', string='Currency', readonly=True, default=lambda self: self.env.company.currency_id)
