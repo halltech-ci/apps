86bbcc12aa801e65d2167306642987a2780deb44
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-06-08 12:20:03 +0000

    Commenting analytic in hta_analytic_report

--- hta_analytic_report/__manifest__.py
+++ hta_analytic_report/__manifest__.py
@@ -27,8 +27,8 @@
        #'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/analytic_report_views.xml',
-        'views/analytic_type_views.xml',
+        #'views/analytic_report_views.xml',
+        #'views/analytic_type_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
