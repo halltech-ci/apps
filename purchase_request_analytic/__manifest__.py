2af0dd7e52d777b3edb51a9f8e5abe5f9d108721
ATCHE Maurice <maurice.atce@halltech-africa.com>
2022-06-15 18:23:49 +0000

    Adding compute function in purchase_request_analytic

--- purchase_request_analytic/__manifest__.py
+++ purchase_request_analytic/__manifest__.py
@@ -16,11 +16,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Purchases Management',
+    'version': '13.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['purchase_request_custom'],
 
     # always loaded
     'data': [
