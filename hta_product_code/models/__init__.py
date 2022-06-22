1ec0d68a6754f2f4e23920a7c006d80f50f723dd
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-06-22 20:15:14 +0000

    Deleting hta_product_code

--- hta_product_code/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-#from . import config_settings
-#from . import code_editor
-#from . import product_category
-#from . import product_attribute
-#from . import product
