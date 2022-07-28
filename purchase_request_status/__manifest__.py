070ea870dfcc473c4a362a0ed2bf85176ed5ef64
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-07-28 22:20:39 +0000

    Adding purchase_request_status

--- purchase_request_status/__manifest__.py
+++ purchase_request_status/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/purchase_request_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
