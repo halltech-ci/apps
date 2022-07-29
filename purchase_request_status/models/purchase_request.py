e9e4286321303b9ae1f79956e7aa7b2b0eeacc60
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-07-29 06:45:06 +0000

    Adding stock_state in purchase_request_status

--- purchase_request_status/models/purchase_request.py
+++ purchase_request_status/models/purchase_request.py
@@ -19,7 +19,7 @@ class PurchaseRequest(models.Model):
                 if req.purchase_count > 0:
                     if all([line.purchase_state == 'done' for line in req.line_ids]):
                         pr_status = 'purchased'
-                    if any([line.purchase_state == 'purchase' for line in req.line_ids]):
+                    elif any([line.purchase_state == 'purchase' for line in req.line_ids]):
                         pr_status = 'partial'
                 else:
                     pr_status = 'no'
@@ -28,16 +28,23 @@ class PurchaseRequest(models.Model):
     @api.depends('line_ids.purchased_qty', 'line_ids.product_qty')
     def _compute_stock_status(self):
         for req in self:
-            #pr_status = 'no'
-            if req.move_count == 0:
-                st_status = 'no'
+            st_status = 'no'
+            if req.move_count > 0:
+                if all([line.stock_state == "done" for line in req.line_ids]):
+                    st_status = 'received'
+                elif any([line.stock_state == "done" for line in req.line_ids]):
+                    st_status = 'partial'
+                elif all([line.stock_state == "draft" for line in req.line_ids]):
+                    st_status = 'no'
             else:
-                st_status = 'received'  
+                st_status = 'no'  
             req.stock_status = st_status
 
 class PurchaseRequestLine(models.Model):
     _inherit = "purchase.request.line"
     
+    
+    stock_state = fields.Selection(compute="_compute_stock_state", string="Purchase Status", selection=lambda self: self.env["stock.move"]._fields["state"].selection, store=True,)
     
     @api.depends("purchase_lines.state", "purchase_lines.order_id.state")
     def _compute_purchase_state(self):
@@ -66,3 +73,19 @@ class PurchaseRequestLine(models.Model):
                 ):
                     temp_purchase_state = "draft"
             rec.purchase_state = temp_purchase_state
+            
+            
+    @api.depends("purchase_request_allocation_ids.stock_move_id.state")
+    def _compute_stock_state(self):
+        for rec in self:
+            st_state = False
+            if rec.purchase_request_allocation_ids.stock_move_id:
+                if all([line.state == "done" for line in rec.purchase_request_allocation_ids.stock_move_id]):
+                    st_state = "done"
+                elif all([line.state == "cancel" for line in rec.purchase_request_allocation_ids.stock_move_id]):
+                    st_state = "cancel"
+                elif any([line.state == 'done' for line in rec.purchase_request_allocation_ids.stock_move_id]):
+                    st_state = "partially_available"
+                elif all([line.state in ("draft", "cancel") for line in rec.purchase_request_allocation_ids.stock_move_id]):
+                    st_state = "draft"
+            rec.stock_state = st_state
