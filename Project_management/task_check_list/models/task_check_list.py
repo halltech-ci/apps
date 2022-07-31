ea4c2324c808ec33c14373f20412d5df48965bc2
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-07-31 23:32:00 +0000

    Adding timesheet in task_check_list in Project_management

--- Project_management/task_check_list/models/task_check_list.py
+++ Project_management/task_check_list/models/task_check_list.py
@@ -6,6 +6,7 @@ from odoo import models, fields, api
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
+    
     @api.depends('task_checklist')
     def checklist_progress(self):
         """:return the value for the check list progress"""
@@ -18,6 +19,7 @@ class ProjectTask(models.Model):
     task_checklist = fields.Many2many('task.checklist', string='Check List')
     checklist_progress = fields.Float(compute=checklist_progress, string='Check list Progress', store=True, recompute=True, default=0.0)
     max_rate = fields.Integer(string='Maximum rate', default=100)
+    check_list = fields.One2many('task.checklist', "task_id",string="Check list")
 
 
 class TaskChecklist(models.Model):
@@ -26,3 +28,8 @@ class TaskChecklist(models.Model):
 
     name = fields.Char(string='Name', required=True)
     description = fields.Char(string='Description')
+    task_id = fields.Many2one("project.task")
+    employee_id = fields.Many2one('hr.employee')
+    check_box = fields.Boolean(default=False)
+    unit_time = fields.Float(string="Duration", default=0, requied=True)
+    
