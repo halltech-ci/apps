f02ade5fcbdb23d6bc2257a0757b7370d071bc92
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-08-02 06:34:35 +0000

    Adding compute_max_rate in task_check_list

--- Project_management/task_check_list/models/task_check_list.py
+++ Project_management/task_check_list/models/task_check_list.py
@@ -7,19 +7,29 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     
-    @api.depends('task_checklist')
+    @api.depends('check_list_ids.unit_time')
     def checklist_progress(self):
         """:return the value for the check list progress"""
         for rec in self:
-            total_len = self.env['task.checklist'].search_count([])
-            check_list_len = len(rec.task_checklist)
-            if total_len != 0:
-                rec.checklist_progress = (check_list_len*100) / total_len
+            total_check = sum(rec.check_list_ids.filtered(lambda l:l.check_box == True).unit_time)
+        if self.max_rate != 0:
+            rec.checklist_progress = total_check / max_rate
+        else:
+            rec.checklist_progress = 0
 
-    task_checklist = fields.Many2many('task.checklist', string='Check List')
+    #task_checklist = fields.Many2many('task.checklist', string='Check List')
     checklist_progress = fields.Float(compute=checklist_progress, string='Check list Progress', store=True, recompute=True, default=0.0)
-    max_rate = fields.Integer(string='Maximum rate', default=100)
-    check_list = fields.One2many('task.checklist', "task_id",string="Check list")
+    max_rate = fields.Integer(string='Maximum rate', compute="_compute_max_rate")
+    check_list_ids = fields.One2many('task.checklist', "task_id", string="Check list")
+    
+    
+    @api.depends('check_list_ids')
+    def _compute_max_rate(self):
+        max_rate = 0
+        for list in self.check_list_ids:
+            max_rate += list.unit_time
+        self.max_rate = max_rate
+        
 
 
 class TaskChecklist(models.Model):
