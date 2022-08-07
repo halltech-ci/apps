ab79fd6730b55431325d024cc604518e8cedb53a
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-08-07 20:31:26 +0000

    Adding _compute_checklist_progress in task_check_list

--- Project_management/task_check_list/models/task_check_list.py
+++ Project_management/task_check_list/models/task_check_list.py
@@ -6,19 +6,8 @@ from odoo import models, fields, api
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    
-    @api.depends('check_list_ids.unit_time')
-    def checklist_progress(self):
-        """:return the value for the check list progress"""
-        for rec in self:
-            total_check = sum(rec.check_list_ids.filtered(lambda l:l.check_box == True).unit_time)
-        if self.max_rate != 0:
-            rec.checklist_progress = total_check / max_rate
-        else:
-            rec.checklist_progress = 0
-
     #task_checklist = fields.Many2many('task.checklist', string='Check List')
-    checklist_progress = fields.Float(compute=checklist_progress, string='Check list Progress', store=True, recompute=True, default=0.0)
+    checklist_progress = fields.Float(compute=_compute_checklist_progress, string='Check list Progress', store=True, recompute=True, default=0.0)
     max_rate = fields.Integer(string='Maximum rate', compute="_compute_max_rate")
     check_list_ids = fields.One2many('task.checklist', "task_id", string="Check list")
     
@@ -29,6 +18,18 @@ class ProjectTask(models.Model):
         for list in self.check_list_ids:
             max_rate += list.unit_time
         self.max_rate = max_rate
+        
+    @api.depends("check_list_ids.unit_time", "max_rate")
+    def _compute_checklist_progress(self):
+        for rec in self:
+            total_check = 0
+            for line in rec.check_list_ids:
+                if line.check_box == True:
+                    total_check += line.unit_time
+            if rec.max_rate != 0:
+                rec.checklist_progress = total_check/rec.max_rate
+            else:
+                rec.checklist_progress = 0
         
 
 
