f80868652d5800239bf75dc85bd4a7e3f55862ca
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-08-08 07:52:40 +0000

    Adding status in task_check_list

--- Project_management/task_check_list/models/task_check_list.py
+++ Project_management/task_check_list/models/task_check_list.py
@@ -10,6 +10,7 @@ class ProjectTask(models.Model):
     checklist_progress = fields.Float(compute="_compute_checklist_progress", string='Check list Progress', store=True, recompute=True, default=0.0)
     max_rate = fields.Integer(string='Maximum rate', compute="_compute_max_rate")
     check_list_ids = fields.One2many('task.checklist', "task_id", string="Check list")
+    task_status = fields.Boolean(string="Status", default=False)
     
     
     @api.depends('check_list_ids')
@@ -27,12 +28,19 @@ class ProjectTask(models.Model):
                 if line.check_box == True:
                     total_check += line.unit_time
             if rec.max_rate != 0:
-                rec.checklist_progress = total_check/rec.max_rate
+                rec.checklist_progress = 100 * total_check/rec.max_rate
             else:
                 rec.checklist_progress = 0
+                
+    @api.onchange("checklist_progress")
+    def _onchange_checklist_progess(self):
+        for task in self:
+            if task.checklist_progress == 100.0:
+                task.task_status = True
+            else:
+                task.checklist_progress = False
+                #task.subtask_effective_hours = sum(child_task.effective_hours + child_task.subtask_effective_hours for child_task in task.child_ids)
         
-
-
 class TaskChecklist(models.Model):
     _name = 'task.checklist'
     _description = 'Checklist for the task'
@@ -43,4 +51,5 @@ class TaskChecklist(models.Model):
     employee_id = fields.Many2one('hr.employee')
     check_box = fields.Boolean(default=False)
     unit_time = fields.Float(string="Duration", default=0, requied=True)
+    date = fields.Date()
     
