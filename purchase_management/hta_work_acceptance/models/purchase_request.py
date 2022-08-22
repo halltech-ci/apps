282b6ceb65e124d832800e3f511ff97686a9c837
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-08-22 10:30:27 +0000

    Adding prestation de service in purchase_management

--- purchase_management/hta_work_acceptance/models/purchase_request.py
+++ purchase_management/hta_work_acceptance/models/purchase_request.py
@@ -7,3 +7,9 @@ class PurchaseRequest(models.Model):
     _inherit = 'purchase.request'
     
     purchase_type = fields.Selection(selection_add=[('service', 'Prestation de service')])
+    
+    
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+    
+    purchase_type = fields.Selection(selection_add=[('service', 'Prestation de service')])
